8e90cfc3e5cbbd11bb34f662da94a0cb7686a1a1
thakurds <thakur.ds.in@gmail.com>
2014-12-21 11:42:50 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,11 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
     set <- function(y) {
